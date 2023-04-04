512a61593638f1894bc100a993035ffa6e61dcf2
Alfred024 <josferast@gmail.com>
2023-04-03 21:15:02 -0600

    Created DoughnutTest component

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout, LineChartTest } from '../components';
+import { Layout, LineChartTest, DoughnutTest } from '../components';
 
 function Home() {
     return (
@@ -8,19 +8,15 @@ function Home() {
               <LineChartTest />
               <LineChartTest />
               <LineChartTest />
-              <LineChartTest />
+              <DoughnutTest />
             </div>
+            
             {/*DIVIDIR PANTALLA EN 4 PARTES */}
 
             {/* //Parte 1: Gráfico de ventas (eje -,+) */}
-
             {/* //Parte 2: Texto con número (dinero) de ventas (eje +,+) */}
-
             {/* //Parte 3: Mejores clientes (eje -,-) */}
-
             {/* //Parte 4: Gráfico de pastel sobre los productos más vendidos (eje +,-) */}
-
-
 
             {/* TEST DE LIBRERÍA ChartJS */}
             
